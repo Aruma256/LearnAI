564e5ecdb11d8b167a75ac7300901e2cd8defeef
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2017-05-16 22:27:26 +0900

    Add basic function for Q in CartPole

--- TD/Q/gym_CartPole.py
+++ TD/Q/gym_CartPole.py
@@ -3,15 +3,36 @@ import gym
 
 def main():
     env = gym.make('CartPole-v0')
+    env.render()
+
+    RIGHT = 1
+    LEFT = 0
+
+    SLOW = 0.01 # 1/SPEED
+
+    def choose_action(state, end):
+        x, dx, w, dw = state
+        return RIGHT
+    
+    def update(state, end, next_state):
+        x, dx, w, dw = state
+        nx, ndx, nw, ndw = next_state
+        pass
+    
     for episode in range(20):
         state = env.reset()
-        for t in range(100):
-            sleep(0.01)
+        done = False
+        t = 0
+        while not done:
+            sleep(SLOW)
             env.render()
-            action = env.action_space.sample()
-            state, reward, done, info = env.step(action)
+            action = choose_action(state, done)         # action = env.action_space.sample()
+            next_state, _, done, _ = env.step(action)   # state, reward, done, info = env.step(action)
+            update(state, done, next_state)
+            state = next_state
+            t += 1
             if done:
-                print("Episode finished after {} timesteps".format(t+1))
+                print("Episode finished after {} timesteps".format(t + 1))
                 sleep(0.5)
                 break
 
